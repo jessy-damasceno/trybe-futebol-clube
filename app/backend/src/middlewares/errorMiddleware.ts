7f9adf27083e9ae5156f893bfda8019bcf2a9555
Jessy Damasceno <jessy_bas@hotmail.com>
2022-10-23 10:27:39 -0300

    feat: implement errorMiddleware in project

---
+++ app/backend/src/middlewares/errorMiddleware.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from 'express';
+import IError from '../interfaces/@error';
+import { mapError } from '../utils/mapError';
+
+const errorMiddleware = (err: IError, _req: Request, res: Response, _next: NextFunction) => {
+  const { type, message } = err;
+  return res.status(mapError(type)).json({ message });
+};
+
+export default errorMiddleware;
