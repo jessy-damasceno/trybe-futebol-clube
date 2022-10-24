06795bccf6c0a065dabd6d5fe6b1c9daabeee000
Jessy Damasceno <jessy_bas@hotmail.com>
2022-10-24 16:10:22 -0300

    feat: login validate

--- app/backend/src/routes/loginRoute.ts
+++ app/backend/src/routes/loginRoute.ts
@@ -4,6 +4,6 @@ import * as loginMiddleware from '../middlewares/loginMiddleware';
 const loginRouter = Router();
 
 loginRouter.route('/')
-  .post(loginMiddleware.fields);
+  .post(loginMiddleware.fields, loginMiddleware.login);
 
 export default loginRouter;
