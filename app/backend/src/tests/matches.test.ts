8b5c0e561796728888076dc281e276a90814b9ee
Jessy Damasceno <jessy_bas@hotmail.com>
2022-10-25 19:25:06 -0300

    feat: GET /matches tests

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -9,6 +9,8 @@ import { Response } from 'superagent';
 
 import {
   allMatchesMock,
+  matchesInProress,
+  matchesNotInProgress,
 } from './mocks/matches.mock';
 
 chai.use(chaiHttp);
@@ -18,33 +20,38 @@ const { expect } = chai;
 describe.only('Teste /matches', () => {
   let chaiHttpResponse: Response;
 
+  before(() => {
+    sinon.stub(Match, "findAll").resolves(allMatchesMock as any[]);
+  })
+
   after(() => {
     (Match.findAll as sinon.SinonStub).restore();
   })
 
   it('Retorna todas as partidas na rota /matches', async () => {
-    sinon.stub(Match, "findAll").resolves(allMatchesMock as any[]);
-
     chaiHttpResponse = await chai
        .request(app)
        .get('/matches');
 
-    console.log(chaiHttpResponse.body);
-    
     expect(chaiHttpResponse.status).to.be.eq(200);
     expect(chaiHttpResponse.body).to.be.deep.eq(allMatchesMock);
   });
 
-  // it('Retorna um time pelo id na rota /teams/:id', async () => {
-  //   sinon.stub(Team, "findByPk").resolves(allTeamsMock[0] as Team);
+  it('Retorna todas as partidas em progresso na rota /matches?inProgress=true', async () => {
+    chaiHttpResponse = await chai
+       .request(app)
+       .get('/matches?inProgress=true');
 
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      .get('/teams/1');
+    expect(chaiHttpResponse.status).to.be.eq(200);
+    expect(chaiHttpResponse.body).to.be.deep.eq(matchesInProress);
+  });
 
-  //   console.log(chaiHttpResponse.body);
-    
-  //   expect(chaiHttpResponse.status).to.be.eq(200);
-  //   expect(chaiHttpResponse.body).to.be.deep.eq(allTeamsMock[0]);
-  // });
+  it('Retorna todas as partidas finalizadas na rota /matches?inProgress=false', async () => {
+    chaiHttpResponse = await chai
+       .request(app)
+       .get('/matches?inProgress=false');
+
+    expect(chaiHttpResponse.status).to.be.eq(200);
+    expect(chaiHttpResponse.body).to.be.deep.eq(matchesNotInProgress);
+  });
 });
