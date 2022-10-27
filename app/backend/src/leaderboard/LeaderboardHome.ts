7f54f1056a95ac3161cdf042288aec0428611f7f
Jessy Damasceno <jessy_bas@hotmail.com>
2022-10-27 19:01:24 -0300

    refatore: constructor with super.initialize method

--- app/backend/src/leaderboard/LeaderboardHome.ts
+++ app/backend/src/leaderboard/LeaderboardHome.ts
@@ -5,26 +5,7 @@ class LeaderboardHome extends Leaderboard {
   constructor(protected matches: IMatch[], teams: ITeam[]) {
     super();
     this.matches.forEach((match) => this.setResults(match));
-
-    Object.keys(this.teamsResults).forEach((team) => {
-      const { totalVictories, totalDraws,
-        goalsFavor, goalsOwn, totalGames, totalLosses } = this.teamsResults[team as never];
-
-      const teamPoints = this.totalPoints(totalVictories, totalDraws);
-
-      this.teamsResults[team as never].totalPoints = teamPoints;
-      this.teamsResults[team as never].totalLosses = totalLosses;
-      this.teamsResults[team as never].totalDraws = totalDraws;
-      this.teamsResults[team as never].totalVictories = totalVictories;
-      this.teamsResults[team as never].goalsBalance = this.goalsBalance(goalsFavor, goalsOwn);
-      this.teamsResults[team as never].efficiency = this.efficiency(teamPoints, totalGames);
-    });
-
-    this.table = teams.map((team) => {
-      this.teamsResults[team.id as number].name = team.teamName;
-
-      return this.teamsResults[team.id as number];
-    }).sort(this.sortTable);
+    super.initialize(matches, teams);
   }
 
   setResults = (match: IMatch) => {
